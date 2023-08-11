2753df7c8ad2a4ef6aeabb9731a6f5a64b274ffe
ChomChanminea <mineachom99@gamail.com>
2023-08-11 17:20:09 +0700

    7 commit

--- server/app.js
+++ server/app.js
@@ -10,11 +10,11 @@ const PORT = 3000;
 app.use('/home', home);
 app.use('/api', api);
 
-app.listen(PORT, (req, res) => { console.log(`This app listen to port: ${PORT}`); });
-// db.connect('mongodb+srv://admin:m190399n@devdestop.zcizub3.mongodb.net/?retryWrites=true&w=majority')
-//     .then(() => {
-//         console.log('connected!');
-//         app.listen(3000, () => {
-//             console.log('this app run on port 3000');
-//         })
-//     }).catch(err => { console.log(err); })
+// app.listen(PORT, (req, res) => { console.log(`This app listen to port: ${PORT}`); });
+db.connect('mongodb+srv://admin:m190399n@devdestop.zcizub3.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('connected!');
+        app.listen(3000, () => {
+            console.log('this app run on port 3000');
+        })
+    }).catch(err => { console.log(err); })
