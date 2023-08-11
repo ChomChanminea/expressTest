dfbfb1dc36cbb93741219fd7d9efc688a0f76307
ChomChanminea <mineachom99@gamail.com>
2023-08-11 12:58:02 +0700

    5 commit

--- server/app.js
+++ server/app.js
@@ -3,13 +3,14 @@ const app = express();
 const home = require('./router/home.js');
 const db = require('mongoose');
 const api = require('./router/api.js')
+const PORT = 3000;
 
 
 
 app.use('/home', home);
 app.use('/api', api);
 
-app.listen(3000);
+app.listen(PORT, (req, res) => { console.log(`This app listen to port: ${PORT}`); });
 // db.connect('mongodb+srv://admin:m190399n@devdestop.zcizub3.mongodb.net/?retryWrites=true&w=majority')
 //     .then(() => {
 //         console.log('connected!');
